598dcfeca613cb3d2ea4754e37260048b621fef2
Christopher Henderson <chris@chenderson.org>
2018-02-17 09:56:28 -0700

    Update unittest.sh

--- unittest.sh
+++ unittest.sh
@@ -6,4 +6,4 @@
 
 # List all test files		# Get the base names (E.G. ServerTests.class --> ServerTests)	# Execute the tests
 ls bin/server/*Tests.class | xargs -I '{}' basename '{}' .class | xargs java -cp "$CLASSPATH:bin/server" org.junit.runner.JUnitCore
-ls bin/client/*Tests.class | xargs -I '{}' basename '{}' .class | xargs java -cp "$CLASSPATH:bin/server" org.junit.runner.JUnitCore
+ls bin/client/*Tests.class | xargs -I '{}' basename '{}' .class | xargs java -cp "$CLASSPATH:bin/client" org.junit.runner.JUnitCore
