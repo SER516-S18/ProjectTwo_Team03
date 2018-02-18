eb1927dcf33700caf96d4f51e824447eb999387c
ygoel <ygoel@asu.edu>
2018-02-18 09:21:00 -0700

    Made Centralized Constants

--- src/Constants.java
+++ src/Constants.java
@@ -2,15 +2,12 @@ import java.awt.Color;
 import java.awt.Font;
 
 public class Constants {
-	public static final Color LIGHT_BLUE = new Color(230, 230, 250);
-	public static final Color LIGHT_RED = new Color(255, 218, 185);
-	public static final Color SKIN = new Color(255, 228, 225);
-	public static final Color BLUE =  new Color(207, 220, 239);
-	public static final Font COURIER_FONT = new Font("Courier New", Font.PLAIN, 18);
-	public static final Color FRAME_COLOR = new Color(173, 216, 230);
-	public static final Color CONSOLE_COLOR = new Color(211, 211, 211);
-	public static final Color MAINPANEL_COLOR = new Color(220, 220, 220);
-	public static final Font BLINKINGTEXT_FONT = new Font("Courier New", Font.BOLD, 20);
+	public static final int PORT_NUMBER = 1516;
+	public static final Color LIGHTBLUE = new Color(228, 233, 239);
+	public static final Color LIGHTRED = new Color(255, 228, 225);
+	public static final Color FRAMECOLOR = new Color(207, 220, 239);
+	public static final Color CONSOLECOLOR = new Color(211, 211, 211);
+	public static final Color MAINPANELCOLOR = new Color(220, 220, 220);
+	public static final Font COURIERFONT = new Font("Courier New", Font.PLAIN, 18);
 	public static final String HOSTNAME = "localhost";
-
 }
