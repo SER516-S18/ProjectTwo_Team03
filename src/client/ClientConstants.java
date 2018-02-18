af1ec5ef0e50ca60b26ed0fc1f43d5da55fc73f5
Vivek Faldu <vivekfaldu14@gmail.com>
2018-02-17 21:29:32 -0700

    Update ClientConstants.java

--- src/client/ClientConstants.java
+++ src/client/ClientConstants.java
@@ -3,9 +3,9 @@ package client;
 public class ClientConstants {
 
 	public static final int PORT_NUMBER = 1516;
-	public static final Color LIGHTBLUE = new Color(228, 233, 239);
-	public static final Color Blue =  new Color(207, 220, 239);
-	public static final Color LIGHTRED = new Color(255, 228, 225);
+	public static final Color LIGHT_BLUE = new Color(228, 233, 239);
+	public static final Color BLUE =  new Color(207, 220, 239);
+	public static final Color LIGHT_RED = new Color(255, 228, 225);
 	public static final Font F1 = new Font("Courier New", Font.PLAIN, 18);
 }
 }
