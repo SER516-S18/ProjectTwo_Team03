dab261b228da007a92e5568ae79e43eb91de6ce2
Utsav Dusad <udusad@asu.edu>
2018-02-17 18:14:56 -0700

    server socket

---
+++ src/server/Server.java
@@ -0,0 +1,69 @@
+
+import java.io.*;
+import java.net.*;
+
+
+
+public class Server {
+	public static void main(String args[]) {
+	
+		Server server = new Server(  ServerConstants.PORT_NUMBER );
+		server.startServer();
+	}
+
+	ServerSocket ServerService = null;
+	Socket clientSocket = null;
+	int port;
+	BufferedReader in;
+     PrintStream out;
+
+	public Server( int port ) {
+		this.port = port;
+	}
+
+	public void stopServer() {
+		System.out.println( "Server cleaning up." );
+		try {
+			ServerService.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.exit(0);
+	}
+
+	public void startServer() {
+
+		try {
+			ServerService = new ServerSocket(port);
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}   
+
+		System.out.println( "Server is ready..." );
+
+
+		while ( true ) {
+			try {
+				clientSocket = ServerService.accept();
+				  in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		           out = new PrintStream(clientSocket.getOutputStream());
+		           String  line ;
+		           while ((  line = in.readLine()) !=null) {
+		        	   	 
+		             out.println(line); 
+		             System.out.println(line);
+		           }
+		           
+			}   
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+
+	
