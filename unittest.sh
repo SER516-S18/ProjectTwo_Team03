3022af3c9a6037aa32df6f5fcb061ce352686194
christopher-henderson <chris@chenderson.org>
2018-02-19 10:24:22 -0700

    fixing package identifier for client tests

--- unittest.sh
+++ unittest.sh
@@ -6,4 +6,4 @@
 
 # List all test files		# Get the base names (E.G. ServerTests.class --> ServerTests)	# Execute the tests
 ls bin/server/*Tests.class | xargs -I '{}' basename '{}' .class | xargs -I '{}' echo server.'{}' | xargs java -cp "$CLASSPATH:bin" org.junit.runner.JUnitCore
-ls bin/client/*Tests.class | xargs -I '{}' basename '{}' .class | xargs -I '{}' echo server.'{}' | xargs java -cp "$CLASSPATH:bin" org.junit.runner.JUnitCore
+ls bin/client/*Tests.class | xargs -I '{}' basename '{}' .class | xargs -I '{}' echo client.'{}' | xargs java -cp "$CLASSPATH:bin" org.junit.runner.JUnitCore
