8e6b18a0c270b1b1da84bb76426c52cdedd803fb
Himanshu Dua <32210683+hdua1@users.noreply.github.com>
2018-02-22 12:15:08 -0700

    Update ClientService.java

--- src/client/ClientService.java
+++ src/client/ClientService.java
@@ -1,16 +1,12 @@
 package client;
 
 public class ClientService {
-
-	public ClientService() {
-		// TODO Auto-generated constructor stub
-		System.out.println("ClientService");
+    public ClientService() {
+	// TODO Auto-generated constructor stub
+	System.out.println("ClientService");
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ClientService clientService = new ClientService();
-
-	}
-
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	ClientService clientService = new ClientService();
+    }
 }
