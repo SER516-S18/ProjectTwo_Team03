79b7585915b2ee23e8b7c56324651ab5c15cd13b
Shubhangi Gupta <shubhangi140793@gmail.com>
2018-02-23 00:36:39 -0700

    removed print in Server UI console and changed package name of CalculateValues.java

--- src/client/CalculateValues.java
+++ src/client/CalculateValues.java
@@ -1,42 +1,43 @@
-package Client;
+package client;
 
 import java.util.List;
 
 public class CalculateValues {
-    
-    private List<Integer> values;
-    /*
-     * Accepts the list of integers to be plotted.
-     */
-    public int HighestValue(List<Integer> values){
-        this.values = values;
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<values.size(); i++){
-            if(values.get(i) > max){
-                max = values.get(i);
-            }
-        }
-        return max;
-    }
-    
-    public int LowestValue(List<Integer> values){
-        this.values = values;
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<values.size(); i++){
-            if(values.get(i) < min){
-                min = values.get(i);
-            }
-        }
-        return min;
-    }
-           
-    public int AverageValue(List<Integer> values){
-        this.values = values;
-        int avg = 0;
-        for(int i=0; i<values.size(); i++){
-            avg+= values.get(i);
-        }
-        avg=avg/values.size();
-        return avg;
-   }
+
+	private List<Integer> values;
+
+	/*
+	 * Accepts the list of integers to be plotted.
+	 */
+	public int HighestValue(List<Integer> values) {
+		this.values = values;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < values.size(); i++) {
+			if (values.get(i) > max) {
+				max = values.get(i);
+			}
+		}
+		return max;
+	}
+
+	public int LowestValue(List<Integer> values) {
+		this.values = values;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < values.size(); i++) {
+			if (values.get(i) < min) {
+				min = values.get(i);
+			}
+		}
+		return min;
+	}
+
+	public int AverageValue(List<Integer> values) {
+		this.values = values;
+		int avg = 0;
+		for (int i = 0; i < values.size(); i++) {
+			avg += values.get(i);
+		}
+		avg = avg / values.size();
+		return avg;
+	}
 }
