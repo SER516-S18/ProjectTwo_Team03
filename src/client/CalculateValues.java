8ffbb9b5bd56ddd919e8b815571fe0da0f677336
swaroope <swaroop297@gmail.com>
2018-02-23 11:39:55 -0700

    Changed the return type of functions in CalculteValues

--- src/client/CalculateValues.java
+++ src/client/CalculateValues.java
@@ -1,43 +1,67 @@
 package client;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class CalculateValues {
 
-	private List<Integer> values;
+    /**
+     * Accepts the list of integers to be plotted.
+     * @param values
+     */
 
-	/*
-	 * Accepts the list of integers to be plotted.
-	 */
-	public int HighestValue(List<Integer> values) {
-		this.values = values;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < values.size(); i++) {
-			if (values.get(i) > max) {
-				max = values.get(i);
-			}
-		}
-		return max;
-	}
 
-	public int LowestValue(List<Integer> values) {
-		this.values = values;
-		int min = Integer.MAX_VALUE;
-		for (int i = 0; i < values.size(); i++) {
-			if (values.get(i) < min) {
-				min = values.get(i);
-			}
-		}
-		return min;
-	}
+    static List<Integer> values = new ArrayList<Integer>();
 
-	public int AverageValue(List<Integer> values) {
-		this.values = values;
-		int avg = 0;
-		for (int i = 0; i < values.size(); i++) {
-			avg += values.get(i);
-		}
-		avg = avg / values.size();
-		return avg;
-	}
+    public CalculateValues(ConcurrentLinkedQueue<String> messageQueue) {
+        String message = messageQueue.poll();
+        String[] messages = message.split(" ");
+        for (int i = 0; i < messages.length; i++) {
+            try {
+                values.add(Integer.parseInt(messages[i]));
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }
+
+    }
+
+    public CalculateValues() {
+
+    }
+
+    public String HighestValue(){
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<values.size(); i++){
+            if(values.get(i) > max){
+                max = values.get(i);
+            }
+        } 
+        System.out.println(max);
+        return String.valueOf(max);
+    }
+
+
+    public String LowestValue(){
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<values.size(); i++){
+            if(values.get(i) < min){
+                min = values.get(i);
+            }
+        }
+        return String.valueOf(min);
+    }
+
+    public String AverageValue(){
+        int avg = 0;
+        if(values.size() != 0) {
+            for(int i=0; i<values.size(); i++){
+                avg+= values.get(i);
+            }
+            avg=avg/values.size();
+            return String.valueOf(avg);
+        }
+        return "0";
+    }
 }
