6a44d7acd52c96d1273dd1300fb16ff1a91b021e
Vivek Faldu <vivekfaldu14@gmail.com>
2018-02-23 00:02:40 -0700

    updated indentation and minor code refactoring

--- src/client/ClientUi.java
+++ src/client/ClientUi.java
@@ -1,4 +1,5 @@
 package client;
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,9 +17,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Component;
 import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Random;
 
 
 /**
@@ -26,91 +24,61 @@ import java.io.IOException;
  * @Version 1.0
  */
 
-public class ClientUi {
-    private JFrame clientFrame;
-    private  Client client=null;
-    
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    ClientUi clientUi = new ClientUi();
-                    clientUi.clientFrame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-	
+public class ClientUi implements ActionListener {
+	private JFrame clientFrame;
+	private Client client = null;
+	JComboBox<String> channelDropDown = null;
 
-    /**
-     * Create the graphical user interface
-     */
-    public ClientUi() {
-        initialize();
-    }
-    /**
-     * Initialize the contents of the GUI.
-     */
-    private void initialize(){
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientUi clientUi = new ClientUi();
+					clientUi.clientFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the graphical user interface
+	 */
+	public ClientUi() {
+		
+		CalculateValues valueObject = new CalculateValues(); 
         clientFrame = new JFrame();
         clientFrame.getContentPane().setBackground(ClientConstants.FRAMECOLOR);
         clientFrame.setTitle("Client");
         clientFrame.setBounds(100, 100, 800, 715);
         clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         clientFrame.getContentPane().setLayout(null);
-        
+
         JButton btnStartStopButton = new JButton("Start / Stop");
         btnStartStopButton.setBackground(ClientConstants.LIGHTRED);
-        btnStartStopButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            if (client == null) {
-                client = new Client(ClientConstants.CHANNELS);
-                client.start();
-            } else if (client.getIsRunning()) {
-                try {
-                    client.stop();
-                    client=null;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Start/Stop");
-            }
-            
-        });
+        btnStartStopButton.addActionListener(this);
         btnStartStopButton.setFont(ClientConstants.COURIERFONT);
         btnStartStopButton.setBounds(580, 13, 190, 34);
         btnStartStopButton.setBorder(BorderFactory.createLineBorder(Color.black));
         clientFrame.getContentPane().add(btnStartStopButton);
-        
+
         JPanel panel = new JPanel();
         panel.setBorder(new LineBorder(new Color(0, 0, 0)));
         panel.setBackground(ClientConstants.MAINPANELCOLOR);
         panel.setBounds(10, 59, 760, 470);
         clientFrame.getContentPane().add(panel);
         panel.setLayout(null);
-        
+
         JPanel graph = new JPanel();
         graph.setBounds(22, 13, 440, 440);
         panel.add(graph);
         graph.setBackground(ClientConstants.LIGHTRED);
         graph.setBorder(BorderFactory.createLineBorder(Color.black));
-        
-//        List<Integer> values = new ArrayList<Integer>();
-//        Random random = new Random();
-//        int maxDataPoints = 16;
-//        int maxScore = 20;
-//        for (int i = 0; i < maxDataPoints ; i++) {
-//           values.add(random.nextInt(maxScore));
-//        }
-//        DisplayGraph g = new DisplayGraph(values);
-//        graph.add(g,BorderLayout.CENTER);
-        
+
         JLabel lblHighValue = new JLabel("<html>Highest<br>value:</html>");
         lblHighValue.setBounds(470, 13, 133, 80);
         panel.add(lblHighValue);
@@ -120,7 +88,7 @@ public class ClientUi {
         lblHighValue.setBorder(BorderFactory.createLineBorder(Color.black));
         lblHighValue.setHorizontalAlignment(SwingConstants.CENTER);
         lblHighValue.setVerticalAlignment(SwingConstants.CENTER);
-        
+
         JLabel lblLowValue = new JLabel("<html>Lowest<br>value:</html>");
         lblLowValue.setBounds(470, 103, 133, 80);
         panel.add(lblLowValue);
@@ -130,7 +98,7 @@ public class ClientUi {
         lblLowValue.setBackground(ClientConstants.LIGHTRED);
         lblLowValue.setHorizontalAlignment(SwingConstants.CENTER);
         lblLowValue.setVerticalAlignment(SwingConstants.CENTER);
-        
+
         JLabel lblAverage = new JLabel("<html>Average:</html>");
         lblAverage.setBounds(470, 193, 133, 80);
         panel.add(lblAverage);
@@ -140,7 +108,6 @@ public class ClientUi {
         lblAverage.setBorder(BorderFactory.createLineBorder(Color.black));
         lblAverage.setBackground(ClientConstants.LIGHTBLUE);
         lblAverage.setOpaque(true);
-        
 
         JLabel lblChannels = new JLabel("<html>Channels:</html>");
         lblChannels.setBounds(470, 283, 133, 80);
@@ -151,9 +118,7 @@ public class ClientUi {
         lblChannels.setBorder(BorderFactory.createLineBorder(Color.black));
         lblChannels.setBackground(ClientConstants.LIGHTRED);
         lblChannels.setOpaque(true);
-        
-        
-        
+
         JLabel lblFrequency = new JLabel("<html>Frequency<br>(Hz):</html>");
         lblFrequency.setBounds(470, 373, 133, 80);
         panel.add(lblFrequency);
@@ -163,9 +128,8 @@ public class ClientUi {
         lblFrequency.setBorder(BorderFactory.createLineBorder(Color.black));
         lblFrequency.setBackground(ClientConstants.LIGHTBLUE);
         lblFrequency.setOpaque(true);
-        
-        
-        JTextPane textHighValue = new JTextPane();  
+
+        JTextPane textHighValue = new JTextPane();
         textHighValue.setAlignmentX(Component.RIGHT_ALIGNMENT);
         textHighValue.setBounds(615, 13, 126, 80);
         panel.add(textHighValue);
@@ -173,7 +137,7 @@ public class ClientUi {
         textHighValue.setBackground(ClientConstants.LIGHTRED);
         textHighValue.setFont(ClientConstants.COURIERFONT);
         textHighValue.setBorder(BorderFactory.createLineBorder(Color.black));
-        
+
         JTextPane textLowValue = new JTextPane();
         textLowValue.setBounds(615, 103, 126, 80);
         panel.add(textLowValue);
@@ -181,16 +145,44 @@ public class ClientUi {
         textLowValue.setBackground(ClientConstants.LIGHTBLUE);
         textLowValue.setFont(ClientConstants.COURIERFONT);
         textLowValue.setBorder(BorderFactory.createLineBorder(Color.black));
-        
-        JTextPane textAvg = new JTextPane(); 
+
+        JTextPane textAvg = new JTextPane();
         textAvg.setBounds(615, 193, 126, 80);
         panel.add(textAvg);
         textAvg.setEditable(false);
         textAvg.setBackground(ClientConstants.LIGHTRED);
         textAvg.setFont(ClientConstants.COURIERFONT);
         textAvg.setBorder(BorderFactory.createLineBorder(Color.black));
-       
-	 Thread t = new Thread(new Runnable() { 
+
+        String[] channels = { "1", "2", "3", "4", "5" };
+
+        channelDropDown = new JComboBox<String>(channels);
+        channelDropDown.setSelectedIndex(1);
+        ClientConstants.CHANNELS = Integer.parseInt(channelDropDown.getSelectedItem().toString());
+        channelDropDown.setBounds(615, 300, 126, 40);
+        panel.add(channelDropDown);
+        channelDropDown.setBackground(ClientConstants.LIGHTBLUE);
+        ((JLabel) channelDropDown.getRenderer()).setHorizontalAlignment(JLabel.RIGHT);
+        channelDropDown.setFont(ClientConstants.COURIERFONT);
+        channelDropDown.setBorder(BorderFactory.createLineBorder(Color.black));
+        channelDropDown.addActionListener(this);
+        
+        JTextPane textFrequency = new JTextPane();
+        textFrequency.setBounds(615, 373, 126, 80);
+        panel.add(textFrequency);
+        textFrequency.setBackground(ClientConstants.LIGHTRED);
+        textFrequency.setFont(ClientConstants.COURIERFONT);
+        textFrequency.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        JTextPane textConsole = new JTextPane();
+        textConsole.setText("Console:");
+        textConsole.setFont(ClientConstants.COURIERFONT);
+        textConsole.setBackground(ClientConstants.CONSOLECOLOR);
+        textConsole.setBounds(10, 550, 760, 100);
+        textConsole.setBorder(BorderFactory.createLineBorder(Color.black));
+        clientFrame.getContentPane().add(textConsole);
+        
+        Thread t = new Thread(new Runnable() { 
 			@Override
 			public void run() {
 				while(true)
@@ -209,48 +201,38 @@ public class ClientUi {
 
 		});
 		t.start();
+		
+	}
 
-        String[] channels = { "1", "2", "3", "4", "5" };
+	@Override
+    public void actionPerformed(ActionEvent ev) {
+        String event = ev.getActionCommand();
+        System.out.println(event);
 
-        JComboBox<String> channelDropDown = new JComboBox<String>(channels);
-        channelDropDown.setSelectedIndex(1);
-        ClientConstants.CHANNELS=Integer.parseInt( channelDropDown.getSelectedItem().toString());
-        channelDropDown.setBounds(615,300,126,40);
-        panel.add(channelDropDown);
-        channelDropDown.setBackground(ClientConstants.LIGHTBLUE);
-        ((JLabel)channelDropDown.getRenderer()).setHorizontalAlignment(JLabel.RIGHT);
-        channelDropDown.setFont(ClientConstants.COURIERFONT);
-        channelDropDown.setBorder(BorderFactory.createLineBorder(Color.black));
-        channelDropDown.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                System.out.print(channelDropDown.getSelectedItem());
-                ClientConstants.CHANNELS=Integer.parseInt( channelDropDown.getSelectedItem().toString());
-                if(client!=null){
-                    try {
-                        client.updateChannels(ClientConstants.CHANNELS);
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
+        if (event.equalsIgnoreCase("comboBoxChanged")) {
+            System.out.print(channelDropDown.getSelectedItem());
+            ClientConstants.CHANNELS = Integer.parseInt(channelDropDown.getSelectedItem().toString());
+            if (client != null) {
+                try {
+                    client.updateChannels(ClientConstants.CHANNELS);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
                 }
             }
-        });
-        
-        JTextPane textFrequency = new JTextPane(); 
-        textFrequency.setBounds(615, 373, 126, 80);
-        panel.add(textFrequency);
-        textFrequency.setBackground(ClientConstants.LIGHTRED);
-        textFrequency.setFont(ClientConstants.COURIERFONT);
-        textFrequency.setBorder(BorderFactory.createLineBorder(Color.black));
-        
-        
-        JTextPane textConsole = new JTextPane();
-        textConsole.setText("Console:");
-        textConsole.setFont(ClientConstants.COURIERFONT);
-        textConsole.setBackground(ClientConstants.CONSOLECOLOR);
-        textConsole.setBounds(10, 550, 760, 100);
-        textConsole.setBorder(BorderFactory.createLineBorder(Color.black));
-        clientFrame.getContentPane().add(textConsole);
-    }
+        }
+
+        else if (event.equalsIgnoreCase("Start / Stop")) {
+            if (client == null || !client.getIsRunning()) {
+                client = new Client(ClientConstants.CHANNELS);
+                client.start();
+            } else if (client.getIsRunning()) {
+                try {
+                    client.stop();
+                    client = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+	}
 }
